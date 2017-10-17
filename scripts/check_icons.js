8fd965baf8545991e2b2c0fcece558ec84e82a24
Gregor MacLennan <gmaclennan@digital-democracy.org>
2017-10-17 10:38:31 -0700

    Ignore missing icons if they are part of the maki icon set used by iD

--- scripts/check_icons.js
+++ scripts/check_icons.js
@@ -1,6 +1,11 @@
 var glob = require('glob')
+var path = require('path')
 
-module.exports = function checkIcons(presets, iconDir) {
+module.exports = function checkIcons (presets, iconDir) {
+  var makiIconPath = path.dirname(require.resolve('maki/src/aerialway-12.svg'))
+  var makiIcons = glob.sync('*-12.svg', {cwd: makiIconPath}).map(filename => {
+    return filename.replace('-12.svg', '')
+  })
   var iconNames = glob.sync('*.svg', {cwd: iconDir}).map(filename => {
     return filename.replace(/-\d+px\.svg$/, '')
   })
@@ -9,7 +14,9 @@ module.exports = function checkIcons(presets, iconDir) {
   Object.keys(presets).forEach(key => {
     var icon = presets[key].icon
     if (!icon) return undefinedIcons.push(key)
-    if (iconNames.indexOf(icon) < 0) missing.push('"' + icon + '" (preset: ' + key + ')')
+    if (iconNames.indexOf(icon) < 0 && makiIcons.indexOf(icon) < 0) {
+      missing.push('"' + icon + '" (preset: ' + key + ')')
+    }
   })
   if (undefinedIcons.length) {
     console.warn('Warning: No icon defined:\n- ' + undefinedIcons.join('\n- ') + '\n')
